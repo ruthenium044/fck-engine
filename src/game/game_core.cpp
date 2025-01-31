93e159b2f13ca9e8da0db290847cb51c62b51355
David <D.Naussed@outlook.de>
2025-01-31 22:14:54 +0100

    Remove application detail for assets

--- src/game/game_core.cpp
+++ src/game/game_core.cpp
@@ -19,10 +19,6 @@ fck_ecs::entity_type game_cammy_create(fck_ecs *ecs)
 	game_size* size = fck_ecs_component_create<game_size>(ecs, cammy);
 	game_debug_colour* color = fck_ecs_component_create<game_debug_colour>(ecs, cammy);
 
-	game_sprite* sprite = fck_ecs_component_create<game_sprite>(ecs, cammy);
-	sprite->texture = gen_assets_png::PestLogo;
-	sprite->src = { 0.0f, 0.0f, 1000.0f, 1000.0f };
-
 	// We just offset the y position based on the entity ID. Good enough
 	position->x = 0.0f;
 	position->y = 128.0f + (cammy * 128);

4eece8f63f4560b4aee34351d53758d1f9782629
Ruthenium <ruthenium@rutheniumsair2.home>
2025-02-01 18:15:18 +0100

    Adds emscripten main loop

--- src/fck.cpp
+++ src/fck.cpp
@@ -7,9 +7,12 @@
 #include <SDL3/SDL.h>
 
 // SDL image - Loads images... Many kinds. We only care about PNG
-#include <core/fck_time.h>
 #include <SDL3_image/SDL_image.h>
+#include <core/fck_time.h>
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 
 void fck_init(fck *core, uint8_t instance_capacity)
 {
@@ -28,30 +31,46 @@ void fck_prepare(fck *core, fck_instance_info const *info, fck_instance_setup_fu
 	fck_instances_add(&core->instances, info, setup_function);
 }
 
+static void fck_tick(fck *core)
+{
+#ifdef __EMSCRIPTEN__
+	if (!fck_instances_any_active(&core->instances))
+	{
+		emscripten_cancel_main_loop(); /* this should "kill" the app. */
+	}
+#endif
+
+	// Maybe global control later
+	fck_milliseconds now = SDL_GetTicks();
+
+	for (fck_instance *instance : &core->instances)
+	{
+		fck_time *time = fck_ecs_unique_view<fck_time>(&instance->ecs);
+		fck_milliseconds tp = time->current;
+		fck_milliseconds delta_time = now - tp;
+
+		time->current = tp;
+		time->delta = delta_time;
+	}
+
+	fck_instances_process_events(&core->instances);
+
+	for (fck_instance *instance : &core->instances)
+	{
+		fck_ecs_tick(&instance->ecs);
+	}
+}
+
 int fck_run(fck *core)
 {
-	fck_milliseconds tp = SDL_GetTicks();
+#ifdef __EMSCRIPTEN__
+	emscripten_set_main_loop_arg((em_arg_callback_func)fck_tick, (void *)core, 0, true);
+#else
 	while (fck_instances_any_active(&core->instances))
 	{
-		// Maybe global control later
-		fck_milliseconds now = SDL_GetTicks();
-		fck_milliseconds delta_time = now - tp;
-		tp = now;
-
-		for (fck_instance *instance : &core->instances)
-		{
-			fck_time *time = fck_ecs_unique_view<fck_time>(&instance->ecs);
-			time->current = tp;
-			time->delta = delta_time;
-		}
-
-		fck_instances_process_events(&core->instances);
-
-		for (fck_instance *instance : &core->instances)
-		{
-			fck_ecs_tick(&instance->ecs);
-		}
+		fck_tick(core);
 	}
+#endif
 
 	// If we get any other return codes or things that require error processing, we should just return it here
 
@@ -63,7 +82,7 @@ void fck_quit(fck *core)
 	SDL_assert(core != nullptr);
 
 	fck_instances_free(&core->instances);
-	
+
 	SDL_QuitSubSystem(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 	SDL_Quit();
 

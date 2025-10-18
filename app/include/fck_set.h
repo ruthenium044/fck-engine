1fe5df599543f3893792d90610ca73b3d1cdc41d
David <D.Naussed@outlook.de>
2025-10-18 20:45:17 +0200

    * Adds cmake folder for ts and std

--- app/include/fck_set.h
+++ app/include/fck_set.h
@@ -3,7 +3,7 @@
 
 #include <fckc_inttypes.h>
 
-#define fck_set_alignof(x) fck_set_suggested_align(sizeof(x))
+// #define fck_set_alignof(x) fck_set_suggested_align(sizeof(x))
 
 // TODO: Can also do a deferred subscript instead...
 #define fck_set_defer_eval(ptr) ((ptr) = (ptr), (ptr))
@@ -14,65 +14,65 @@ struct fck_set_state;
 // Not a fan of making this public, but whatever
 typedef struct fck_set_info
 {
+	fckc_size_t cookie;
 	struct kll_allocator *allocator;
 	fckc_size_t capacity;
 	fckc_size_t size;
-	fckc_size_t el_align;
+	// Let's fuck this for now - max_align of this is 64 bytes
+	// fckc_size_t el_align;
 	fckc_size_t el_size;
 	fckc_size_t stale;
-
 	struct fck_set_key *keys;
 	struct fck_set_state *states;
 } fck_set_info;
 
 fckc_size_t fck_set_suggested_align(fckc_size_t x);
 
-fck_set_info *fck_opaque_set_inspect(void *ptr, fckc_size_t align);
+fck_set_info *fck_opaque_set_inspect(void const *ptr);
 
 void *fck_opaque_set_alloc(fck_set_info info);
-void fck_opaque_set_free(void *ptr, fck_set_info *info);
-void fck_opaque_set_clear(void *ptr, fck_set_info *info);
+void fck_opaque_set_free(void *ptr);
+void fck_opaque_set_clear(void *ptr);
 
-fckc_size_t fck_opaque_set_strong_add(void **ptr, fckc_u64 hash, fck_set_info *info);
-fckc_size_t fck_opaque_set_weak_add(void **ptr, fckc_u64 hash, fck_set_info *info);
-fckc_size_t fck_opaque_set_find(void *ptr, fckc_u64 hash, fck_set_info *info);
-void fck_opaque_set_remove(void *ptr, fckc_u64 hash, fck_set_info *info);
+fckc_size_t fck_opaque_set_strong_add(void **ptr, fckc_u64 hash);
+fckc_size_t fck_opaque_set_weak_add(void **ptr, fckc_u64 hash);
+fckc_size_t fck_opaque_set_find(void *ptr, fckc_u64 hash);
+void fck_opaque_set_remove(void *ptr, fckc_u64 hash);
 
-fckc_size_t fck_opaque_set_begin(void const *ptr, fck_set_info const *info);
-int fck_opaque_set_next(fck_set_info const *info, fckc_size_t *index);
+fckc_size_t fck_opaque_set_begin(void const *ptr);
+int fck_opaque_set_next(void const *ptr, fckc_size_t *index);
 
-int fck_opaque_set_valid_at(void const *ptr, fck_set_info const *info, fckc_size_t at);
-struct fck_set_key *fck_opaque_set_keys_at(void const *ptr, fck_set_info const *info, fckc_size_t at);
+int fck_opaque_set_valid_at(void const *ptr, fckc_size_t at);
+struct fck_set_key *fck_opaque_set_keys_at(void const *ptr, fckc_size_t at);
 
 fckc_u64 fck_set_key_resolve(struct fck_set_key *key);
 
 // Let's see if ptr to ptr makes sense
-#define fck_set_inspect(ptr) fck_opaque_set_inspect((void *)(ptr), (fck_set_alignof(*(ptr))))
+#define fck_set_inspect(ptr) fck_opaque_set_inspect((void *)(ptr))
 #define fck_set_new(type, alloc, cap)                                                                                                      \
-	(type *)fck_opaque_set_alloc(                                                                                                          \
-		(fck_set_info){.allocator = (alloc), .el_align = fck_set_alignof(type), .el_size = sizeof(type), .capacity = (cap)})
-#define fck_set_destroy(ptr) fck_opaque_set_free((void *)(ptr), fck_set_inspect((ptr)))
-#define fck_set_clear(ptr) fck_opaque_set_clear(ptr, (fck_set_inspect((ptr))))
+	(type *)fck_opaque_set_alloc((fck_set_info){.allocator = (alloc), .el_size = sizeof(type), .capacity = (cap)})
+
+#define fck_set_destroy(ptr) fck_opaque_set_free((void *)(ptr))
+#define fck_set_clear(ptr) fck_opaque_set_clear(ptr)
 
 // TODO: A bit stronger type for index/iterator
-#define fck_set_begin(ptr) fck_opaque_set_begin(ptr, fck_set_inspect((ptr)))
-#define fck_set_next(ptr, iterator) fck_opaque_set_next(fck_set_inspect((ptr)), &(iterator))
-
-// TODO: Maybe making this a strong_add might be more appropiate
-#define fck_set_add(ptr, hash, value)                                                                                                      \
-	fck_opaque_set_weak_add((void **)&(ptr), hash, (fck_set_inspect((ptr))))[(fck_set_defer_eval(ptr))] = value
-#define fck_set_remove(ptr, hash) fck_opaque_set_remove((void *)(ptr), hash, (fck_set_inspect((ptr))))
+#define fck_set_begin(ptr) fck_opaque_set_begin(ptr)
+#define fck_set_next(ptr, iterator) fck_opaque_set_next((ptr), &(iterator))
 
 // fck_set_at(set, hash(key)) = value;
-#define fck_set_at(ptr, hash) fck_opaque_set_weak_add((void **)&(ptr), hash, (fck_set_inspect((ptr))))[(fck_set_defer_eval(ptr))]
+#define fck_set_at(ptr, hash) fck_opaque_set_weak_add((void **)&(ptr), hash)[(ptr)]
+
+// TODO: Maybe making this a strong_add might be more appropiate
+#define fck_set_add(ptr, hash, value) fck_set_at(ptr, hash) = value
+#define fck_set_remove(ptr, hash) fck_opaque_set_remove((void *)(ptr), hash)
 
 //	fckc_size_t has = fck_set_probe(set, hash(k));
 //	if(has) {
 //		set[has - 1] = value;
 //	}
-#define fck_set_find(ptr, hash) fck_opaque_set_find((void *)(ptr), hash, (fck_set_inspect((ptr))))
+#define fck_set_find(ptr, hash) fck_opaque_set_find((void *)(ptr), hash)
 
-#define fck_set_valid_at(ptr, at) fck_opaque_set_valid_at((ptr), fck_set_inspect((ptr)), (at))
-#define fck_set_keys_at(ptr, at) fck_opaque_set_keys_at((ptr), fck_set_inspect((ptr)), (at))
+#define fck_set_valid_at(ptr, at) fck_opaque_set_valid_at((ptr), (at))
+#define fck_set_keys_at(ptr, at) fck_opaque_set_keys_at((ptr), (at))
 #define fck_set_index_of(ptr, entry) (fckc_size_t)((entry) - (ptr))
 #endif // FCK_SET_H_INCLUDED
--- app/src/fck_set.c
+++ app/src/fck_set.c
@@ -7,6 +7,7 @@
 
 #define fck_set_pointer_offset(type, ptr, offset) ((type *)(((fckc_u8 *)(ptr)) + (offset)))
 
+
 // typedef enum fck_set_key_state
 //{
 //	FCK_SET_KEY_EMPTY = 0LLU, // 00
@@ -14,6 +15,7 @@
 //	FCK_SET_KEY_STALE = 2LLU, // 10 (11 is unsed... - 1 wasted bit)
 // } fck_set_key_state;
 
+#define FCK_SET_KEY_COOKIE ((fckc_size_t)0xDEAD537)
 #define FCK_SET_KEY_EMPTY 0LLU
 #define FCK_SET_KEY_TAKEN 1LLU
 #define FCK_SET_KEY_STALE 2LLU
@@ -46,55 +48,56 @@ fckc_size_t fck_set_suggested_align(fckc_size_t x)
 	return x;
 }
 
-fck_set_info *fck_opaque_set_inspect(void *ptr, fckc_size_t align)
+fck_set_info *fck_opaque_set_inspect(void const *ptr)
 {
 	fck_set_info *info;
-
-	const fckc_size_t info_size = sizeof(*info);
-	fckc_size_t info_offset = fck_set_align(0, fck_set_alignof(fck_set_info));
-	fckc_size_t data_offset = fck_set_align(info_offset + info_size, align);
-	return fck_set_pointer_offset(fck_set_info, ptr, -data_offset);
+	info = fck_set_pointer_offset(fck_set_info, ptr, -sizeof(*info));
+	SDL_assert(info->cookie == FCK_SET_KEY_COOKIE);
+	return info;
 }
 
-void *fck_opaque_set_alloc(fck_set_info info)
+void *fck_opaque_set_alloc(fck_set_info input)
 {
-	SDL_assert(info.el_size);
-	SDL_assert(info.el_align);
-	SDL_assert(info.allocator);
+	SDL_assert(input.el_size);
+	// SDL_assert(info.el_align);
+	SDL_assert(input.allocator);
 	// SDL_assert(data);
 
-	fck_set_info *info_out;
-	const fckc_size_t info_size = sizeof(*info_out);
-	const fckc_size_t data_size = info.el_size * info.capacity;
-	const fckc_size_t key_size = sizeof(*info_out->keys) * info.capacity;
+	fck_set_info *info;
+	const fckc_size_t info_size = sizeof(*info);
+	const fckc_size_t data_size = input.el_size * input.capacity;
+	const fckc_size_t key_size = sizeof(*info->keys) * input.capacity;
 	// NOTE: Not so pretty implementation detail
-	const fckc_size_t state_size = sizeof(*info_out->states) * ((info.capacity / 32) + 1);
+	const fckc_size_t state_size = sizeof(*info->states) * ((input.capacity / 32) + 1);
 
-	fckc_size_t info_offset = fck_set_align(0, fck_set_alignof(fck_set_info));
-	fckc_size_t data_offset = fck_set_align(info_offset + info_size, info.el_align);
-	fckc_size_t keys_offset = fck_set_align(data_offset + data_size, fck_set_alignof(fck_set_key));
-	fckc_size_t states_offset = fck_set_align(keys_offset + key_size, fck_set_alignof(fck_set_state));
-	info_out = (fck_set_info *)kll_malloc(info.allocator, states_offset + state_size);
+	fckc_size_t info_offset = fck_set_align(0, sizeof(*info));
+	fckc_size_t data_offset = info_offset + info_size; // We piggyback on the alignment of info... somehow., idk
+	fckc_size_t keys_offset = fck_set_align(data_offset + data_size, sizeof(fck_set_key));
+	fckc_size_t states_offset = fck_set_align(keys_offset + key_size, sizeof(fck_set_state));
+	info = (fck_set_info *)kll_malloc(input.allocator, states_offset + state_size);
 
-	SDL_memcpy(info_out, &info, sizeof(*info_out));
+	SDL_memcpy(info, &input, sizeof(*info));
 	// We just remember it for debugging
-	info_out->keys = fck_set_pointer_offset(fck_set_key, info_out, keys_offset);
-	info_out->states = fck_set_pointer_offset(fck_set_state, info_out, states_offset);
-	info_out->stale = 0;
-	SDL_memset(info_out->states, 0, state_size);
+	info->keys = fck_set_pointer_offset(fck_set_key, info, keys_offset);
+	info->states = fck_set_pointer_offset(fck_set_state, info, states_offset);
+	info->stale = 0;
+	SDL_memset(info->states, 0, state_size);
+	info->cookie = FCK_SET_KEY_COOKIE;
 
-	return fck_set_pointer_offset(void, info_out, data_offset);
+	return fck_set_pointer_offset(void, info, data_offset);
 }
 
-void fck_opaque_set_free(void *ptr, fck_set_info *info)
+void fck_opaque_set_free(void *ptr)
 {
 	SDL_assert(ptr);
+	fck_set_info *info = fck_opaque_set_inspect(ptr);
 	kll_free(info->allocator, info);
 }
 
-void fck_opaque_set_clear(void *ptr, fck_set_info *info)
+void fck_opaque_set_clear(void *ptr)
 {
 	SDL_assert(ptr);
+	fck_set_info *info = fck_opaque_set_inspect(ptr);
 	const fckc_size_t state_size = sizeof(*info->states) * ((info->capacity / 32) + 1);
 	SDL_memset(info->states, 0, state_size);
 	info->size = 0;
@@ -124,7 +127,7 @@ static void fck_opaque_set_rehash(fck_set_info **info_ref, void **ptr)
 		void *old = *ptr;
 		void *new = fck_opaque_set_alloc(input_info);
 
-		fck_set_info *new_info = fck_opaque_set_inspect(new, input_info.el_align);
+		fck_set_info *new_info = fck_opaque_set_inspect(new);
 		fck_set_key *new_keys = new_info->keys;
 
 		for (fckc_size_t index = 0; index < info->capacity; index++)
@@ -138,15 +141,15 @@ static void fck_opaque_set_rehash(fck_set_info **info_ref, void **ptr)
 			}
 
 			fck_set_key *old_key = info->keys + index;
-			fckc_size_t at = fck_opaque_set_strong_add(&new, old_key->hash, new_info);
-			new_info = fck_opaque_set_inspect(new, input_info.el_align);
-			fckc_u8* dst = fck_set_pointer_offset(fckc_u8, new, info->el_size * at);
-			fckc_u8* src = fck_set_pointer_offset(fckc_u8, old, info->el_size * index);
+			fckc_size_t at = fck_opaque_set_strong_add(&new, old_key->hash);
+			new_info = fck_opaque_set_inspect(new);
+			fckc_u8 *dst = fck_set_pointer_offset(fckc_u8, new, info->el_size * at);
+			fckc_u8 *src = fck_set_pointer_offset(fckc_u8, old, info->el_size * index);
 			SDL_memcpy(dst, src, info->el_size);
 		}
 
 		new_info->size = info->size;
-		fck_opaque_set_free(*ptr, info);
+		fck_opaque_set_free(*ptr);
 
 		// Refresh all pointers...
 		*ptr = new;
@@ -154,11 +157,12 @@ static void fck_opaque_set_rehash(fck_set_info **info_ref, void **ptr)
 	}
 }
 
-fckc_size_t fck_opaque_set_weak_add(void **ptr, fckc_u64 hash, fck_set_info *info)
+fckc_size_t fck_opaque_set_weak_add(void **ptr, fckc_u64 hash)
 {
 	SDL_assert(ptr);
 	SDL_assert(*ptr);
 
+	fck_set_info *info = fck_opaque_set_inspect(*ptr);
 	fck_opaque_set_rehash(&info, ptr);
 
 	fckc_size_t at = hash % info->capacity;
@@ -192,22 +196,24 @@ fckc_size_t fck_opaque_set_weak_add(void **ptr, fckc_u64 hash, fck_set_info *inf
 	}
 }
 
-fckc_size_t fck_opaque_set_strong_add(void **ptr, fckc_u64 hash, fck_set_info *info)
+fckc_size_t fck_opaque_set_strong_add(void **ptr, fckc_u64 hash)
 {
 	SDL_assert(ptr);
 	SDL_assert(*ptr);
 
-	fckc_size_t has = fck_opaque_set_find(*ptr, hash, info);
+	fckc_size_t has = fck_opaque_set_find(*ptr, hash);
 	SDL_assert(!has);
-	fckc_size_t at = fck_opaque_set_weak_add(ptr, hash, info);
+	fckc_size_t at = fck_opaque_set_weak_add(ptr, hash);
 	return at;
 }
 
-void fck_opaque_set_remove(void *ptr, fckc_u64 hash, fck_set_info *info)
+void fck_opaque_set_remove(void *ptr, fckc_u64 hash)
 {
 	SDL_assert(ptr);
 
-	fckc_size_t has = fck_opaque_set_find(ptr, hash, info);
+	fck_set_info *info = fck_opaque_set_inspect(ptr);
+
+	fckc_size_t has = fck_opaque_set_find(ptr, hash);
 	if (has)
 	{
 		fckc_size_t at = has - 1;
@@ -223,10 +229,11 @@ void fck_opaque_set_remove(void *ptr, fckc_u64 hash, fck_set_info *info)
 	}
 }
 
-fckc_size_t fck_opaque_set_find(void *ptr, fckc_u64 hash, fck_set_info *info)
+fckc_size_t fck_opaque_set_find(void *ptr, fckc_u64 hash)
 {
 	SDL_assert(ptr);
 
+	fck_set_info *info = fck_opaque_set_inspect(ptr);
 	fckc_size_t at = hash % info->capacity;
 	for (;;)
 	{
@@ -261,10 +268,11 @@ fckc_u64 fck_set_key_resolve(fck_set_key *key)
 	return key->hash;
 }
 
-int fck_opaque_set_valid_at(void const *ptr, fck_set_info const *info, fckc_size_t at)
+int fck_opaque_set_valid_at(void const *ptr, fckc_size_t at)
 {
 	SDL_assert(ptr);
-	SDL_assert(info);
+
+	fck_set_info* info = fck_opaque_set_inspect(ptr);
 
 	fck_set_state *state = info->states + (at / 32);
 	fckc_u64 taken_mask = FCK_SET_KEY_TAKEN << ((at % 32) * 2);
@@ -272,24 +280,27 @@ int fck_opaque_set_valid_at(void const *ptr, fck_set_info const *info, fckc_size
 	return has_value;
 }
 
-fck_set_key *fck_opaque_set_keys_at(void const *ptr, fck_set_info const *info, fckc_size_t at)
+fck_set_key *fck_opaque_set_keys_at(void const *ptr, fckc_size_t at)
 {
 	SDL_assert(ptr);
-	SDL_assert(info);
+
+	fck_set_info* info = fck_opaque_set_inspect(ptr);
 
 	return info->keys + at;
 }
 
-fckc_size_t fck_opaque_set_begin(void const *ptr, fck_set_info const *info)
+fckc_size_t fck_opaque_set_begin(void const *ptr)
 {
 	SDL_assert(ptr);
-	SDL_assert(info);
 	return (fckc_size_t)(-1LLU);
 }
 
-int fck_opaque_set_next(fck_set_info const *info, fckc_size_t *index)
+int fck_opaque_set_next(void const *ptr, fckc_size_t *index)
 {
-	SDL_assert(info);
+	SDL_assert(ptr);
+
+	fck_set_info *info = fck_opaque_set_inspect(ptr);
+
 	*index = *index + 1;
 	for (;;)
 	{

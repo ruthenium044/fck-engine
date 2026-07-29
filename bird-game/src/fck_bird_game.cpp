af5acb94dba39100722ea7af33d9f3dcf65c2aa8
Ruthenium <rutaspk@gmail.com>
2026-07-29 18:43:17 +0200

    * Adds iterators

--- bird-game/src/fck_bird_game.cpp
+++ bird-game/src/fck_bird_game.cpp
@@ -40,6 +40,14 @@ void *operator new(std::size_t size, kll_allocator *alloc)
 	return memory;
 }
 
+void operator delete(void *ptr, kll_allocator *alloc)
+{
+	if(ptr)
+	{
+		kll_free(alloc, ptr);
+	}
+}
+
 static fck_gameloop fck_bird_game_create(kll_allocator *allocator, const fck_gameloop_create_parameters *params)
 {
 	fck_bird_game *game = new (allocator) fck_bird_game();
@@ -52,7 +60,7 @@ static fck_gameloop fck_bird_game_create(kll_allocator *allocator, const fck_gam
 static void fck_bird_game_destroy(fck_gameloop loop, const fck_gameloop_destroy_parameters *params)
 {
 	fck_bird_game *game = fck_to_bird_game(loop);
-	kll_free(game->allocator, game);
+	delete game;
 }
 
 static int fck_bird_game_edit(fck_gameloop loop, const fck_gameloop_edit_parameters *params)
@@ -72,33 +80,32 @@ static int fck_bird_game_edit(fck_gameloop loop, const fck_gameloop_edit_paramet
 template <typename T>
 struct fck_component_view
 {
-	struct Iterator
+	struct iterator
 	{
-		const fck_entity *entities;
+		const fck_entity *current;
 		T *data;
-		fckc_u32 index;
 
-		struct Reference
+		struct reference
 		{
 			fck_entity entity;
 			T &component_data;
 		};
 
-		Reference operator*() const
+		reference operator*() const
 		{
-			const auto entity = entities[index];
-			return {entity, data[entity.index]};
+			return {*current, data[current->index]};
 		}
 
-		Iterator &operator++()
+		iterator &operator++()
 		{
-			++index;
+			current++;
 			return *this;
 		}
 
-		bool operator!=(const Iterator &other) const
+		bool operator!=(const iterator &other) const
 		{
-			return index != other.index;
+			// In usual forward iteration, other->current == entities + count => end
+			return current != other.current;
 		}
 	};
 
@@ -106,18 +113,95 @@ struct fck_component_view
 	T *data;
 	fckc_u32 count;
 
-	Iterator begin() const
+	iterator begin() const
 	{
-		return {entities, data, 0};
+		return {entities + 0, data};
 	}
-	Iterator end() const
+	iterator end() const
 	{
-		return {entities, data, count};
+		return {entities + count, data};
+	}
+};
+
+struct fck_component_entity_view
+{
+	struct iterator
+	{
+		const fck_entity *current;
+
+		fck_entity operator*() const
+		{
+			return *current;
+		}
+
+		iterator &operator++()
+		{
+			current++;
+			return *this;
+		}
+
+		bool operator!=(const iterator &other) const
+		{
+			return current != other.current;
+		}
+	};
+
+	const fck_entity *entities;
+	fckc_u32 count;
+
+	iterator begin() const
+	{
+		return {entities + 0};
+	}
+	iterator end() const
+	{
+		return {entities + count};
 	}
 };
 
 template <typename T>
-static auto fck_make_view(fck_ec *state, fck_ec_api *ec, const char *component_name)
+struct fck_component_data_view
+{
+	struct iterator
+	{
+		const fck_entity *current;
+		T *data;
+
+		T &operator*() const
+		{
+			return data[current->index];
+		}
+
+		iterator &operator++()
+		{
+			current++;
+			return *this;
+		}
+
+		bool operator!=(const iterator &other) const
+		{
+			// In usual forward iteration, other->current == entities + count => end
+			return current != other.current;
+		}
+	};
+
+	const fck_entity *entities;
+	T *data;
+	fckc_u32 count;
+
+	iterator begin() const
+	{
+		return {entities + 0, data};
+	}
+	iterator end() const
+	{
+		return {entities + count, data};
+	}
+};
+
+
+template <typename T>
+static fck_component_view<T> fck_make_view(fck_ec *state, fck_ec_api *ec, const char *component_name)
 {
 	const fck_component_id comp_id = ec->registry->id(*state, component_name);
 	const fck_entity *entities;
@@ -125,6 +209,26 @@ static auto fck_make_view(fck_ec *state, fck_ec_api *ec, const char *component_n
 	T *data = reinterpret_cast<T *>(ec->component->buffer(*state, comp_id));
 
 	return fck_component_view<T>{entities, data, count};
+}
+
+static auto fck_make_entity_view(fck_ec *state, fck_ec_api *ec, const char *component_name)
+{
+	const fck_component_id comp_id = ec->registry->id(*state, component_name);
+	const fck_entity *entities;
+	const fckc_u32 count = ec->component->dense(*state, comp_id, &entities);
+
+	return fck_component_entity_view{entities, count};
+}
+
+template <typename T>
+static auto fck_make_component_view(fck_ec *state, fck_ec_api *ec, const char *component_name)
+{
+	const fck_component_id comp_id = ec->registry->id(*state, component_name);
+	const fck_entity *entities;
+	const fckc_u32 count = ec->component->dense(*state, comp_id, &entities);
+	T *data = reinterpret_cast<T *>(ec->component->buffer(*state, comp_id));
+
+	return fck_component_data_view<T>{entities, data, count};
 }
 
 static int fck_bird_game_tick(fck_gameloop loop, const fck_gameloop_tick_parameters *params)
@@ -137,6 +241,20 @@ static int fck_bird_game_tick(fck_gameloop loop, const fck_gameloop_tick_paramet
 	{
 		os->io->log("%d %d", id.batch.value, id.entry.value);
 	}
+
+	for (auto id : fck_make_component_view<fck_sprite_id>(params->state, params->ec, "sprite"))
+	{
+		os->io->log("%d %d", id.batch.value, id.entry.value);
+	}
+
+	for (auto entity : fck_make_entity_view(params->state, params->ec, "sprite"))
+	{
+		os->io->log("%d %d", entity.generation, entity.index);
+	}
+
+	std::vector<int> vec;
+	std::vector<float> data;
+
 	return 1;
 }
 
